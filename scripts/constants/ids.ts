5acbc7c36b418bf6e53ee5b9676a4180aa9e2c27
spo0ds <asmitadhikari540@gmail.com>
2023-11-30 13:08:23 +0545

    constants required for the dapp

--- scripts/constants/ids.ts
+++ scripts/constants/ids.ts
@@ -1,7 +1,7 @@
 export const packageId = "";
 export const schoolRecordId = "";
-export const Cap = "";
-export const mnemonic = "";
+export const cap = "";
+export const mnemonic = ""; // better to have it in .env
 export const childId = "";
 export const account = "";
 export const packagePath = "";
