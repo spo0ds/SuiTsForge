0914f01ca5cf5ec12fe60d8a5d11b40b9b5ef6f7
spo0ds <asmitadhikari540@gmail.com>
2023-11-30 12:31:34 +0545

    ids stored in one file

---
+++ scripts/constants/ids.ts
@@ -0,0 +1,7 @@
+export const packageId = "";
+export const schoolRecordId = "";
+export const Cap = "";
+export const mnemonic = "";
+export const childId = "";
+export const account = "";
+export const packagePath = "";
